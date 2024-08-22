4a249d91023bc7c3036310598c4d25b014b5420e
Eliseo Martelli <me@eliseomartelli.it>
2024-08-22 19:55:21 +0200

    fix(ui): change WindowGroup to Window
    Fixes #4.

--- Cleeb/CleebApp.swift
+++ Cleeb/CleebApp.swift
@@ -14,43 +14,32 @@ struct CleebApp: App {
     @State var showAlert: Bool = false
     
     var body: some Scene {
-        WindowGroup {
-            ZStack {
-                CleebView()
-            }
-            .alert(isPresented: $showAlert, content: {
-                Alert(
-                    title: Text("Cleeb requires Accessibility Permissions"),
-                    message: Text("""
+        Window("Cleeb", id: "cleeb") {
+            CleebView()
+                .alert(isPresented: $showAlert, content: accessibilityAlert)
+                .checkAccessibility(interval: 1, isPermitted: $isPermitted)
+                .onChange(of: isPermitted) {
+                    showAlert = !isPermitted && !showAlert
+                }
+        }
+        .windowResizability(.contentSize)
+    }
+    
+    let accessibilityAlert: () -> Alert = {
+        Alert(
+            title: Text("Cleeb requires Accessibility Permissions"),
+            message: Text("""
 Cleeb needs to access Accessibility API in order to lock the keyboard while \
 cleaning.
 Please open System Preferences > Security & Privacy > Accessibility and make \
 sure Cleeb has Accessibility Permissions.
 """),
-                    primaryButton: Alert.Button.default(
-                        Text("Open Settings")
-                        , action: {
-                            AccessibilityUtils.openAccessibilitySettings()
-                        }),
-                    secondaryButton: Alert.Button.cancel(
-                        Text("Dismiss")
-                    )
-                )
-            })
-            .checkAccessibility(interval: 1, isPermitted: $isPermitted)
-            .onAppear {
-                NSWindow.allowsAutomaticWindowTabbing = false
-            }
-            .onChange(of: isPermitted) {
-                if !isPermitted && !showAlert {
-                    showAlert = true
-                }
-            }
-        }
-        .commands {
-            CommandGroup(replacing: .newItem) {
-            }
-        }
-        .windowResizability(.contentSize)
+            primaryButton: Alert.Button.default(
+                Text("Open Settings"),
+                action: {
+                    AccessibilityUtils.openAccessibilitySettings()
+                }),
+            secondaryButton: Alert.Button.cancel(Text("Dismiss"))
+        )
     }
 }
